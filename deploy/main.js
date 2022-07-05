d0997dc54a63f0474da8da7b2926b3da24bd0002
BhryanS2 <bhryanstepenhen@gmail.com>
2022-07-05 01:29:14 -0300

    img

---
+++ deploy/main.js
@@ -0,0 +1,64 @@
+window.addEventListener("scroll", onScroll);
+
+onScroll();
+
+function onScroll() {
+  showNavOnScroll();
+  showBackToTopButtonOnScroll();
+  const sections = [home, services, about, contact];
+  sections.forEach((section) => activateMenuAtCurrentSection(section));
+}
+
+function activateMenuAtCurrentSection(section) {
+  const targetLine = scrollY + innerHeight / 2;
+  const sectionTop = section.offsetTop;
+  const sectionHeight = section.offsetHeight;
+  const sectionTopReachOrPassedTargetline = targetLine >= sectionTop;
+
+  const sectionEndsAt = sectionTop + sectionHeight;
+  const sectionEndPassedTargetline = sectionEndsAt <= targetLine;
+
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetline && !sectionEndPassedTargetline;
+
+  const sectionId = section.getAttribute("id");
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`);
+
+  menuElement.classList.remove("active");
+  if (sectionBoundaries) {
+    menuElement.classList.add("active");
+  }
+}
+
+function showNavOnScroll() {
+  const isScroll = scrollY > 0;
+  navigation.classList.toggle("scroll", isScroll);
+}
+
+function showBackToTopButtonOnScroll() {
+  const isScroll = scrollY > 0;
+  backToTopButton.classList.toggle("show", isScroll);
+}
+
+function openMenu() {
+  document.body.classList.add("menu-expanded");
+}
+
+function closeMenu() {
+  document.body.classList.remove("menu-expanded");
+}
+
+ScrollReveal({
+  origin: "top",
+  distance: "30px",
+  duration: 700,
+}).reveal(`
+  #home, 
+  #home img, 
+  #home .stats, 
+  #services,
+  #services header,
+  #services .card
+  #about, 
+  #about header, 
+  #about .content`);
